407bce427a87d1f74a11ffdc0132c5267782e4bd
Gabriela <gabisgamba@gmail.com>
2025-01-28 14:28:06 -0300

    create the databaseSetUp and the dockerfile

---
+++ backend/populateTables.sql
@@ -0,0 +1,15 @@
+INSERT INTO Admin (token, name) VALUES
+('9zE59WgKc4b/1MNWiLfxc8Utu/f/YtqzScozOAi8QR?uOnCfmOu!JGiYLUcDuE4fO==A?meh-49mNxAkM3?dK/ktZIDQ=KLGnaovwMDC1mtt7dGXArzfDtZ-VKH/nI?U', 'Vitor Arruda'),
+('onqQDRUKtzUfe4!I4WC-xSc!Ki-sQ0EpiMz!JJqi?lHB4eS0xPxfoLcgtqyUcOl-D3KA4c-VMfs-ya5rwyQesPieiZDSaRDLLRU8Nz7!rE9awTsc0Q0-1cd!fNOoarj8', 'Lillian Hendricks');
+
+-- Populando a tabela Corrector
+INSERT INTO Corrector (name) VALUES
+('Cole Mccarthy'),
+('Enya Palmer'),
+('Jaden Gaines');
+
+-- Populando a tabela Corrections
+INSERT INTO Corrections (correctorId, class, module, meeting, student) VALUES
+(1, 'Turma 6', 'Javascript', 'Aula 10 - Métodos funcionais de array', 'Todd Holden'),
+(1, 'Turma 6', 'Javascript', 'Aula 11 - Promises e Async/Await', 'Pearl Davila'),
+(2, 'Turma 5', 'HTML e CSS', 'Aula 3 - Flexbox', 'Allen Norton');
--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const pool = new Pool({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    port: parseInt(process.env.DB_PORT || '5432', 10),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
